5c68fe35d139dfc99f0ab75709318ec09ec43abe
Anton Chernov <malikalhack@gmail.com>
2022-10-03 00:15:53 +0300

    Added a function to calculate the logarithm

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 ﻿#include <stdio.h>
 
-//logarithm is the degree to which the base must be raised to get the argument 
+
+/* logarithm is the degree to which the base must be raised to get the argument */
+unsigned int my_log2(unsigned int arg) {
+    unsigned int result = 0u;
+    if (arg) {
+        if (arg & (arg - 1u)) {
+            printf("The value has been rounded: ");
+        }
+        while (arg > 1u) {
+            arg >>= 1u;
+            result++;
+        }
+    }
+    else printf("Invalid input: ");
+    return result;
+}
+
 int main() {
+    unsigned int res = my_log2(0u);
+    printf("Result is %u\n", res);
+    res = my_log2(1u);
+    printf("Result is %u\n", res);
+    res = my_log2(8u);
+    printf("Result is %u\n", res);
+    res = my_log2(32u);
+    printf("Result is %u\n", res);
+    res = my_log2(35u);
+    printf("Result is %u\n", res);
     getchar();
     return 0;
 }
+
